ec7ed37844e12d338d11ea21500971fcbcf5c26b
Yomi Ikuru <captyomjnr@gmail.com>
2024-04-02 15:25:27 +0100

    feat: add nix flake

--- flake.nix
+++ flake.nix
@@ -4,7 +4,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
+      };
+    };
   };
 
   outputs = { self, nixpkgs, flake-utils, rust-overlay } @inputs:
@@ -15,12 +21,13 @@
           {
             inherit system overlays;
           };
+        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
       in
       with pkgs;
       {
         devShells.default = mkShell {
           buildInputs = [
-            rust-bin.stable.latest.default
+            rustToolchain
           ];
         };
       }
