b23055a9285c67ed4e50337fbb57276fb8542595
Yomi Ikuru <captyomjnr@gmail.com>
2024-11-13 12:50:33 +0000

    feat(python): add initial solutions

---
+++ python/solutions/easy/p1_two_sum.py
@@ -0,0 +1,24 @@
+"""
+Leetcode: https://leetcode.com/problems/two-sum/
+"""
+
+
+def two_sum(nums, target):
+    num_to_index = {}
+    for i, num in enumerate(nums):
+        complement = target - num
+        if complement in num_to_index:
+            return [num_to_index[complement], i]
+        num_to_index[num] = i
+    return []
+
+
+def test_two_sum():
+    test_cases = [
+        ([2, 7, 11, 15], 9, [0, 1]),
+        ([3, 2, 4], 6, [1, 2]),
+        ([3, 3], 6, [0, 1]),
+    ]
+
+    for nums, target, expected in test_cases:
+        assert two_sum(nums, target) == expected
