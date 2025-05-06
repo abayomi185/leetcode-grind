29bd93903a1368612e8249c632c44dbca64ac8df
Yomi Ikuru <captyomjnr@gmail.com>
2025-05-06 13:28:14 +0000

    feat(python): add python solution template

---
+++ python/solutions/template.py
@@ -0,0 +1,18 @@
+"""
+Leetcode: https://leetcode.com/problems/problem_name/
+"""
+
+from utils import TestCase
+
+
+def problem_name(nums, target):
+    return nums + target
+
+
+def test_problem_name():
+    test_cases: list[TestCase] = [
+        TestCase(input=[2, 7, 11, 15], target=9, expected=[0, 1]),
+    ]
+
+    for case in test_cases:
+        assert problem_name(case.input, case.target) == case.expected
