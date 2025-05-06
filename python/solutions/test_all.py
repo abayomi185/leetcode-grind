442af9a2160bce5b0a7f0d8288e33c54b38d55ef
Yomi Ikuru <captyomjnr@gmail.com>
2025-05-06 13:27:36 +0000

    feat(python): add test_all.py and remove test_easy.py

---
+++ python/solutions/test_all.py
@@ -0,0 +1,9 @@
+from solutions.easy import test_all_easy
+from solutions.hard import test_all_hard
+from solutions.medium import test_all_medium
+
+
+def test_all():
+    test_all_easy()
+    test_all_medium()
+    test_all_hard()
--- python/solutions/test_easy.py
+++
@@ -1,5 +0,0 @@
-from solutions.easy.p1_two_sum import test_two_sum
-
-
-def test_all_easy():
-    test_two_sum()
