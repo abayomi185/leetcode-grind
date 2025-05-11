d33acbc64e260773a54c93e041f2567bbb06658e
Yomi Ikuru <captyomjnr@gmail.com>
2025-05-11 22:58:46 +0100

    feat(python): p242 - valid anagram

--- python/solutions/easy/__init__.py
+++ python/solutions/easy/__init__.py
@@ -1,7 +1,9 @@
 from solutions.easy.p1_two_sum import test_two_sum
 from solutions.easy.p217_contains_duplicate import test_contains_duplicate
+from solutions.easy.p242_valid_anagram import test_valid_anagram
 
 
 def test_all_easy():
     test_two_sum()  # P2
     test_contains_duplicate()  # P217
+    test_valid_anagram()  # P242
--- python/solutions/easy/p242_valid_anagram.py
+++ python/solutions/easy/p242_valid_anagram.py
@@ -2,17 +2,46 @@
 Leetcode: https://leetcode.com/problems/valid-anagram/
 """
 
+from typing import Counter
+
 from utils import TestCase
 
 
-def valid_anagram(nums, target):
-    return nums + target
+def valid_anagram(s: str, t: str):
+    if len(s) != len(t):
+        return False
+
+    s_character_map = {}
+    t_character_map = {}
+
+    for s_char, t_char in zip(s, t):
+        s_character_map.setdefault(s_char, 0)
+        t_character_map.setdefault(t_char, 0)
+
+        s_character_map[s_char] += 1
+        t_character_map[t_char] += 1
+
+    for s_key in s_character_map.keys():
+        if not t_character_map.get(s_key):
+            return False
+
+        if s_character_map[s_key] != t_character_map[s_key]:
+            return False
+
+    return True
+
+
+def valid_anagram_2(s: str, t: str):
+    return Counter(s) == Counter(t)
 
 
 def test_valid_anagram():
     test_cases: list[TestCase] = [
-        TestCase(input=[2, 7, 11, 15], target=9, expected=[0, 1]),
+        TestCase(input="racecar", target="carrace", expected=True),
+        TestCase(input="jar", target="jam", expected=False),
+        TestCase(input="aa", target="bb", expected=False),
     ]
 
     for case in test_cases:
-        assert valid_anagram(case.input, case.target) == case.expected
+        assert valid_anagram(str(case.input), str(case.target)) == case.expected
+        assert valid_anagram_2(str(case.input), str(case.target)) == case.expected
