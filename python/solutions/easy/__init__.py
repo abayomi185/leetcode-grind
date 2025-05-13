5b3b509124e4bab911102901de15f510689f9827
Yomi Ikuru <captyomjnr@gmail.com>
2025-05-13 14:11:51 +0100

    wip(python): p125 - valid palindrome

--- python/solutions/easy/__init__.py
+++ python/solutions/easy/__init__.py
@@ -1,4 +1,5 @@
 from solutions.easy.p1_two_sum import test_two_sum
+from solutions.easy.p125_valid_palindrome import test_valid_palindrome
 from solutions.easy.p217_contains_duplicate import test_contains_duplicate
 from solutions.easy.p242_valid_anagram import test_valid_anagram
 
@@ -7,3 +8,4 @@ def test_all_easy():
     test_two_sum()  # P2
     test_contains_duplicate()  # P217
     test_valid_anagram()  # P242
+    test_valid_palindrome()  # P125
---
+++ python/solutions/easy/p125_valid_palindrome.py
@@ -0,0 +1,28 @@
+"""
+Leetcode: https://leetcode.com/problems/valid-palindrome/
+"""
+
+import re
+
+from utils import TestCase
+
+
+def valid_palindrome(input: str):
+    string = re.sub(r"\W+", "", input.lower())
+    reverse_string = string[::-1]
+
+    for pointer in range(0, len(string)):
+        if string[pointer] != reverse_string[pointer]:
+            return False
+
+    return True
+
+
+def test_valid_palindrome():
+    test_cases: list[TestCase] = [
+        TestCase(input="Was it a car or a cat I saw?", expected=True),
+        TestCase(input="tab a cat", expected=False),
+    ]
+
+    for case in test_cases:
+        assert valid_palindrome(str(case.input)) == case.expected
