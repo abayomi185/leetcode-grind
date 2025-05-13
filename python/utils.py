83ca95a059fc6ef78b0dd60f82032b485b71b2de
Yomi Ikuru <captyomjnr@gmail.com>
2025-05-13 13:53:40 +0000

    feat(python/utils): update TestCase class

--- python/utils.py
+++ python/utils.py
@@ -5,5 +5,4 @@ from typing import Any, Optional
 @dataclass
 class TestCase:
     input: Optional[Any] = field(default=None)
-    target: Optional[Any] = field(default=None)
     expected: Optional[Any] = field(default=None)
